ea39da41318440e1be78625c85547ebc97860acc
Rachel Diaz <Rachel7113@gmail.com>
2021-01-25 11:01:17 -0500

    Time-blocks displaying local storage content

--- assets/script.js
+++ assets/script.js
@@ -5,41 +5,37 @@ $("#currentDay").html(currentTime);
 //use document ready function to prevent any jQuery -
 //code from running before the document is finished loading (is ready)
 $(document).ready(function(){
-// Save the text to local storage
-
+   
+    // Save the text to local storage on click
     $(".saveBtn").on("click", function(){
-       
-    var userInput = $(".saveBtn").siblings(".description").val();
-    var hour = $(".saveBtn").parent().attr("id"); 
+    //set key and value variables
+    //using save-btn only selects hour 8 - no matter which btn is clicked
+    //this selects time-block that is clicked
+    var userInput = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id"); 
       //saving data to local storage ("key", "value")
         localStorage.setItem(hour, userInput);
-        // localStorage.setItem("#hour9", userInput);
-        // localStorage.setItem("#hour10", userInput);
-        // localStorage.setItem("#hour11", userInput);
-        // localStorage.setItem("#hour12", userInput);
-        // localStorage.setItem("#hour13", userInput);
-        // localStorage.setItem("#hour14", userInput);
-        // localStorage.setItem("#hour15", userInput);
-        // localStorage.setItem("#hour16", userInput);
-        // localStorage.setItem("#hour17", userInput);
+        
         console.log(localStorage);
+        console.log(userInput);
     })
+//set descriptions from storage per hour
+$("#hour8 .description").val(localStorage.getItem('hour8'));
+$("#hour9 .description").val(localStorage.getItem('hour9'));
+$("#hour10 .description").val(localStorage.getItem('hour10'));
+$("#hour11 .description").val(localStorage.getItem('hour11'));
+$("#hour12 .description").val(localStorage.getItem('hour12'));
+$("#hour13 .description").val(localStorage.getItem('hour13'));
+$("#hour14 .description").val(localStorage.getItem('hour14'));
+$("#hour15 .description").val(localStorage.getItem('hour15'));
+$("#hour16 .description").val(localStorage.getItem('hour16'));
+$("#hour17 .description").val(localStorage.getItem('hour17'));
+console.log(localStorage);
 
-// //set description from storage
-// $(".description").innerText.val(localStorage.getItem("#hour8"));
-// $(".description").innerText.val(localStorage.getItem("#hour9"));
-// $(".description").innerText.val(localStorage.getItem("#hour10"));
-// $(".description").innerText.val(localStorage.getItem("#hour11"));
-// $(".description").innerText.val(localStorage.getItem("#hour12"));
-// $(".description").innerText.val(localStorage.getItem("#hour13"));
-// $(".description").innerText.val(localStorage.getItem("#hour15"));
-// $(".description").innerText.val(localStorage.getItem("#hour16"));
-// $(".description").innerText.val(localStorage.getItem("#hour17"));
 
 
 
 
-// console.log(userInput);
 
 
 
