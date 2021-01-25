94b1dad756461b969c67a2b1fc2e888f1c5047d1
Rachel Diaz <Rachel7113@gmail.com>
2021-01-24 21:49:25 -0500

    saves to local storage

--- assets/script.js
+++ assets/script.js
@@ -6,37 +6,40 @@ $("#currentDay").html(currentTime);
 //code from running before the document is finished loading (is ready)
 $(document).ready(function(){
 // Save the text to local storage
-var userInput = $(".description").val();
 
-    $(".saveBtn").click(function(){
-       //saving data to local storage ("key", "value") 
-        localStorage.setItem("#hour8", userInput);
-        localStorage.setItem("#hour9", userInput);
-        localStorage.setItem("#hour10", userInput);
-        localStorage.setItem("#hour11", userInput);
-        localStorage.setItem("#hour12", userInput);
-        localStorage.setItem("#hour13", userInput);
-        localStorage.setItem("#hour14", userInput);
-        localStorage.setItem("#hour15", userInput);
-        localStorage.setItem("#hour16", userInput);
-        localStorage.setItem("#hour17", userInput);
+    $(".saveBtn").on("click", function(){
+       
+    var userInput = $(".saveBtn").siblings(".description").val();
+    var hour = $(".saveBtn").parent().attr("id"); 
+      //saving data to local storage ("key", "value")
+        localStorage.setItem(hour, userInput);
+        // localStorage.setItem("#hour9", userInput);
+        // localStorage.setItem("#hour10", userInput);
+        // localStorage.setItem("#hour11", userInput);
+        // localStorage.setItem("#hour12", userInput);
+        // localStorage.setItem("#hour13", userInput);
+        // localStorage.setItem("#hour14", userInput);
+        // localStorage.setItem("#hour15", userInput);
+        // localStorage.setItem("#hour16", userInput);
+        // localStorage.setItem("#hour17", userInput);
+        console.log(localStorage);
     })
 
-//set description from storage
-$(".description").innerText = localStorage.getItem("#hour8");
-$(".description").innerText = localStorage.getItem("#hour9");
-$(".description").innerText = localStorage.getItem("#hour10");
-$(".description").innerText = localStorage.getItem("#hour11");
-$(".description").innerText = localStorage.getItem("#hour12");
-$(".description").innerText = localStorage.getItem("#hour13");
-$(".description").innerText = localStorage.getItem("#hour15");
-$(".description").innerText = localStorage.getItem("#hour16");
-$(".description").innerText = localStorage.getItem("#hour17");
+// //set description from storage
+// $(".description").innerText.val(localStorage.getItem("#hour8"));
+// $(".description").innerText.val(localStorage.getItem("#hour9"));
+// $(".description").innerText.val(localStorage.getItem("#hour10"));
+// $(".description").innerText.val(localStorage.getItem("#hour11"));
+// $(".description").innerText.val(localStorage.getItem("#hour12"));
+// $(".description").innerText.val(localStorage.getItem("#hour13"));
+// $(".description").innerText.val(localStorage.getItem("#hour15"));
+// $(".description").innerText.val(localStorage.getItem("#hour16"));
+// $(".description").innerText.val(localStorage.getItem("#hour17"));
 
 
 
-console.log(localStorage);
-console.log(userInput);
+
+// console.log(userInput);
 
 
 
