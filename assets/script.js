f788990dd5c2e6b1ae0a577f6840dfe62d1c850b
Rachel Diaz <Rachel7113@gmail.com>
2021-01-25 14:01:54 -0500

    Corrected tense to show css at proper times

--- assets/script.js
+++ assets/script.js
@@ -36,26 +36,91 @@ console.log(localStorage);
 function tenses (){
     //variable for current time
     var currentHour = moment().hour();
-    console.log(currentHour)
-//function to add css colors to times
-    $(".row").each(function(){
-         if (currentHour == currentHour) {
-             $(".row").addClass("present");
-         } 
-        
-    })
+    console.log(currentHour);
+//.each function cannot be used selects all blocks
+//if statement to add css colors to times
+if (currentHour > 8) {
+            $("#hour8").addClass("past");
+    } else if (currentHour >= 8 && currentHour < 9) {
+        $("#hour8").addClass("present");
+    } else if (currentHour < 8) {
+        $("#hour8").addClass("future");
+    }
+
+if (currentHour > 9) {
+        $("#hour9").addClass("past");
+    } else if (currentHour >= 9 && currentHour < 10) {
+        $("#hour9").addClass("present");
+    } else if (currentHour < 9) {
+        $("#hour9").addClass("future");
+    }   
+
+if (currentHour > 10) {
+        $("#hour10").addClass("past");
+    } else if (currentHour >= 10 && currentHour < 11) {
+        $("#hour10").addClass("present");
+    } else if (currentHour < 10) {
+        $("#hour10").addClass("future");
+    } 
+
+if (currentHour > 11) {
+        $("#hour11").addClass("past");
+    } else if (currentHour >= 11 && currentHour < 12) {
+        $("#hour11").addClass("present");
+    } else if (currentHour < 11) {
+        $("#hour11").addClass("future");
+    } 
     
+if (currentHour > 12) {
+        $("#hour12").addClass("past");
+    } else if (currentHour >= 12 && currentHour < 13) {
+        $("#hour12").addClass("present");
+    } else if (currentHour < 12) {
+        $("#hour12").addClass("future");
+    }
+    
+if (currentHour > 13) {
+        $("#hour13").addClass("past");
+    } else if (currentHour >= 13 && currentHour < 14) {
+        $("#hour13").addClass("present");
+    } else if (currentHour < 13) {
+        $("#hour13").addClass("future");
+    }
+    
+if (currentHour > 14) {
+        $("#hour14").addClass("past");
+    } else if (currentHour >= 14 && currentHour < 15) {
+        $("#hour14").addClass("present");
+    } else if (currentHour < 14) {
+        $("#hour14").addClass("future");
+    } 
+
+if (currentHour > 15) {
+        $("#hour15").addClass("past");
+    } else if (currentHour >= 15 && currentHour < 16) {
+        $("#hour15").addClass("present");
+    } else if (currentHour < 15) {
+        $("#hour15").addClass("future");
+    } 
+
+if (currentHour > 16) {
+        $("#hour16").addClass("past");
+    } else if (currentHour >= 16 && currentHour < 17) {
+        $("#hour16").addClass("present");
+    } else if (currentHour < 16) {
+        $("#hour16").addClass("future");
+    }
+
+if (currentHour > 17) {
+        $("#hour17").addClass("past");
+    } else if (currentHour >= 17 && currentHour < 18) {
+        $("#hour17").addClass("present");
+    } else if (currentHour < 17) {
+        $("#hour17").addClass("future");
+    }
+
 }
-tenses();
 
+    tenses();
 
-
-
-
-
-
-
-
-
-
-});
+})
